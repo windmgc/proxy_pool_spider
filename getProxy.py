8e4cc85b1a1bcaa33773386fe6faa12b60c11b31
WindMGC <windmgc@gmail.com>
2014-12-30 23:11:52 +0800

    Get Proxies from Pachong.org

--- getProxy.py
+++ getProxy.py
@@ -1,22 +1,62 @@
 import urllib2
 from sgmllib import SGMLParser
 
-class ProxyIP (SGMLParser):
+class getProxyList (SGMLParser):
     def __init__(self):
         SGMLParser.__init__(self)
         self.is_td = ""
-        self.name = []
+        self.content = []
     def start_td(self,attrs):
         self.is_td = 1
     def end_td(self):
         self.is_td = ""
     def handle_data(self, text):
         if self.is_td == 1:
-            self.name.append(text)
+            self.content.append(text)
 
-content = urllib2.urlopen('http://pachong.org/').read()
-proxies = ProxyIP()
+# class getTBODY (SGMLParser):
+#     def __init__(self):
+#         SGMLParser.__init__(self)
+#         self.content = []
+#         self.is_tbody = ""
+#     def start_tbody(self,attrs):
+#         self.is_tbody = 1
+#     def end_tbody(self):
+#         self.is_tbody = ""
+#     def handle_data(self, text):
+#         if self.is_tbody == 1:
+#             self.content.append(text)
+
+# class getANIMALS ():
+#     def __init__(self):
+#         self.rawData=""
+#     def getanimals(self):
+
+
+content = urllib2.urlopen('http://pachong.org/area/short/name/cn.html').read()
+
+fp = open("test.txt",'w')
+fp.write(content)
+fp.close()
+
+fp1 = open("test.txt",'r')
+fpcontent=fp1.readlines()
+# print fpcontent[12][31:-10]
+animals=fpcontent[12][31:-10].split(';')
+print animals
+for i in animals:
+    i=i[4:]
+    print i
+    exec(i)
+fp1.close()
+
+proxies = getProxyList()
 proxies.feed(content)
-for item in proxies.name:
-    # print item.decode('gbk').encode('utf8')
-    print item
+# print proxies.content[9]
+# print proxies.content[10]
+# print proxies.content
+# fp2 = open("test.txt",'w')
+# for i in proxies.content:
+#     print i
+#     fp2.write(i)
+# fp2.close()

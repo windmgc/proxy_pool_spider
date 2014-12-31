a43effe9fb1e916816861b01233148a3fa1f03d3
WindMGC <windmgc@gmail.com>
2014-12-31 16:04:28 +0800

    Pachong.org Complete

--- getProxy.py
+++ getProxy.py
@@ -1,62 +1,57 @@
 import urllib2
-from sgmllib import SGMLParser
+import re
+# from sgmllib import SGMLParser
 
-class getProxyList (SGMLParser):
-    def __init__(self):
-        SGMLParser.__init__(self)
-        self.is_td = ""
-        self.content = []
-    def start_td(self,attrs):
-        self.is_td = 1
-    def end_td(self):
-        self.is_td = ""
-    def handle_data(self, text):
-        if self.is_td == 1:
-            self.content.append(text)
-
-# class getTBODY (SGMLParser):
+# class getProxyList (SGMLParser):
 #     def __init__(self):
 #         SGMLParser.__init__(self)
+#         self.is_td = ""
 #         self.content = []
-#         self.is_tbody = ""
-#     def start_tbody(self,attrs):
-#         self.is_tbody = 1
-#     def end_tbody(self):
-#         self.is_tbody = ""
+#     def start_td(self,attrs):
+#         self.is_td = 1
+#     def end_td(self):
+#         self.is_td = ""
 #     def handle_data(self, text):
-#         if self.is_tbody == 1:
+#         if self.is_td == 1:
 #             self.content.append(text)
 
-# class getANIMALS ():
-#     def __init__(self):
-#         self.rawData=""
-#     def getanimals(self):
-
-
-content = urllib2.urlopen('http://pachong.org/area/short/name/cn.html').read()
+htmlContent = urllib2.urlopen('http://pachong.org/').read()
+print "Successfully Read HTML"
 
 fp = open("test.txt",'w')
-fp.write(content)
+fp.write(htmlContent)
 fp.close()
 
 fp1 = open("test.txt",'r')
 fpcontent=fp1.readlines()
-# print fpcontent[12][31:-10]
 animals=fpcontent[12][31:-10].split(';')
-print animals
 for i in animals:
     i=i[4:]
-    print i
     exec(i)
+print "Successfully Read Animals"
 fp1.close()
 
-proxies = getProxyList()
-proxies.feed(content)
-# print proxies.content[9]
-# print proxies.content[10]
-# print proxies.content
-# fp2 = open("test.txt",'w')
-# for i in proxies.content:
+ips=[]
+ipsreg='<td>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}<\/td>'
+ips=re.findall(ipsreg,htmlContent)
+# counter=1
+# for i in ips:
 #     print i
-#     fp2.write(i)
-# fp2.close()
+#     print counter
+#     counter+=1
+print "Successfully Read IPs"
+
+rawports=[]
+ports=[]
+portsreg='<td><script>\s*.*<\/script><\/td>'
+rawports=re.findall(portsreg,htmlContent)
+# counter=1
+# for i in ports:
+#     print i[26:-16]
+#     print counter
+#     counter+=1
+for i in rawports:
+    exec("ports.append("+i[26:-15]+")")
+print "Successfully Read Ports"
+# for i in ports2:
+#     print i

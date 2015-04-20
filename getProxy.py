b83b3c2b024dd1220b92378f0de487041321b14d
WindMGC <windmgc@gmail.com>
2015-04-20 22:08:45 +0800

    Useless Forecheck Deleted

--- getProxy.py
+++ getProxy.py
@@ -52,25 +52,25 @@ def getLatestProxy(newUrl):
     return rawips
 
 def checkOneProxyExist(proxyAddress):
-    testUrl = "http://www.baidu.com/"
-    testProxy = urllib2.ProxyHandler({'http':proxyAddress.split('\n')[0]})
-    testOpener = urllib2.build_opener(testProxy)
-    urllib2.install_opener(testOpener)
-    t1=time.time()
-    if(testUrl.find("?")==-1):
-        testUrl=testUrl+'?rnd='+str(random.random())
-    else:
-        testUrl=testUrl+'&rnd='+str(random.random())
-    try:
-        f = urllib2.urlopen(testUrl,timeout=3)
-    except:
-        pass
-    t2=time.time()
-    timeUsed=t2-t1
-    if timeUsed-3 < 0:
-        if int(RedisIO.hexists("proxy_pool", proxyAddress)) == 0:
-            RedisIO.hset("proxy_pool", proxyAddress, "0")
-            print "add"+proxyAddress
+    # testUrl = "http://www.baidu.com/"
+    # testProxy = urllib2.ProxyHandler({'http':proxyAddress.split('\n')[0]})
+    # testOpener = urllib2.build_opener(testProxy)
+    # urllib2.install_opener(testOpener)
+    # t1=time.time()
+    # if(testUrl.find("?")==-1):
+    #     testUrl=testUrl+'?rnd='+str(random.random())
+    # else:
+    #     testUrl=testUrl+'&rnd='+str(random.random())
+    # try:
+    #     f = urllib2.urlopen(testUrl,timeout=3)
+    # except:
+    #     pass
+    # t2=time.time()
+    # timeUsed=t2-t1
+    # if timeUsed-3 < 0:
+    if int(RedisIO.hexists("proxy_pool", proxyAddress)) == 0:
+        RedisIO.hset("proxy_pool", proxyAddress, "0")
+        # print "add"+proxyAddress
 
 if __name__ == '__main__':
     cleanUp()
